f321195d5b26f61ea9734d7b9f6cc5966438c28d
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2023-10-18 14:55:22 +0100

    create db via sqlalchemy in main.py

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
